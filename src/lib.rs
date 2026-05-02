a4f9f816b900df68a38a5f7ce9c7aafe5c4c64b9
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-05-02 12:51:06 +0700

    feat: static_env_var

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
-//! This is a module-level comment for a Rust lib
+//! This crate provides a [`static_env_var!`] macro for loading the environment variables statically in a `LazyLock`.
 
 #![deny(unused_crate_dependencies)]
+
+#[macro_export]
+macro_rules! static_env_var {
+    ($vis:vis $name:ident) => {
+        $vis static $name: std::sync::LazyLock<String> = std::sync::LazyLock::new(|| std::env::var(stringify!($name)).expect(concat!(stringify!($name), " must be set")));
+    };
+}
